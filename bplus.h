991a35316b03ba9de8d05001851ef05acad8229c
cinna-zju <cinna@yangshuodeMacBook-Air.local>
2017-05-24 21:42:29 +0800

    insert and split
    Next: search

--- bplus.h
+++ bplus.h
@@ -4,7 +4,7 @@
 #include <map>
 #include <iostream>
 
-#define MAX_SIZE 5
+#define size 5
 using namespace std;
 
 typedef long offset;
@@ -14,36 +14,36 @@ template <typename T>
 class Node{
 public:
     int num;//num of
-    int size;
     T *key;
     offset *ptr;
     bool isLeaf;
 
-    Node():num(0), isLeaf(false){
+    Node(){
         // size = (4096 - sizeof(int) - sizeof(bool) - sizeof(offset))
         //             /(sizeof(T) + sizeof(offset));
-        size = 5;
         key = (T*)malloc(sizeof(T)*size);
         ptr = (offset*)malloc(sizeof(offset)*size);
+
     }
+
     void write(offset addr, FILE* fp){
         fseek(fp, addr, SEEK_SET);
-        fwrite(&size, sizeof(int), 1, fp);
         fwrite(&num, sizeof(int), 1, fp);
-        fwrite(key, sizeof(T), num, fp);
-        fwrite(ptr, sizeof(offset), num+1, fp);
+        fwrite(&isLeaf, sizeof(bool), 1, fp);
+        fwrite(key, sizeof(T), size, fp);
+        fwrite(ptr, sizeof(offset), size+1, fp);
     };
 
     void read(offset addr, FILE* fp){
         fseek(fp, addr, SEEK_SET);
-        fread(&size, sizeof(int), 1, fp);
         fread(&num, sizeof(int), 1, fp);
-        fread(key, sizeof(T), num, fp);
-        fread(ptr, sizeof(offset), num+1, fp);
+        fread(&isLeaf, sizeof(bool), 1, fp);
+        fread(key, sizeof(T), size, fp);
+        fread(ptr, sizeof(offset), size+1, fp);
     };
 
     void print(){
-        cout<<size<<" "<<num<<endl;
+        //cout<<size<<" "<<num<<endl;
         for(int i = 0; i < num; i++){
             cout<<key[i]<<' ';
         }
@@ -53,19 +53,6 @@ public:
             cout<<ptr[i]<<' ';
         }
         cout<<endl;
-    }
-
-    void insert(T val , offset addr, int idx){
-        for(int i = num; i >= idx; i--){
-            key[i] = key[i-1];
-            ptr[i+1] = ptr[i];
-        }
-        key[idx] = val;
-        ptr[idx+1] = addr;
-        num++;
-        if(num == size){
-            //split();
-        }
     }
 
 };
@@ -79,20 +66,30 @@ public:
     FILE *Bptfile;
     FILE *RecFile;
 
+    void init(){
+        root = newNode();
+        //cout<<"root addr:"<<root<<endl;
+        Node<T> r;
+        r.ptr[size] = 0;
+        r.num = 0;
+        r.isLeaf = true;
+        r.write(root, Bptfile);
+    };
 
-    void write(Node<T> &r){
-        r.write(0, Bptfile);
-    }
-    void init(){Node<T> r; write(r);};
-    offset newNode();
+    offset newNode(){
+        fseek(Bptfile, 0, SEEK_END);
+        return ftell(Bptfile);
+    };
 
 public:
     bPlusTree(string);
     ~bPlusTree(){fclose(Bptfile);};
 
+    void insert(T, offset, offset);
     void insert(T, offset);
     void split(Node<T>&, Node<T>&, int);
     void print(offset);
+    void printLeaf();
 
 
     pair<offset, offset> search(T, Node<T>);
@@ -103,17 +100,167 @@ public:
 template <typename T>
 bPlusTree<T>::bPlusTree(string filename)
 {
-    root = 0;
     fileName = filename;
 
-    if( (Bptfile = fopen(fileName.c_str(), "rb+")) == NULL ){
+    if((Bptfile = fopen(fileName.c_str(), "wb+"))){
+        cout << "file opened" << endl;
         init();
-        Bptfile = fopen(fileName.c_str(), "rb+");
+    }else{
+        cout << "file error" << endl;
+        exit(-1);
     }
 
 
 }
 
+template <typename T>
+void bPlusTree<T>::insert(T val, offset addr){
+
+    Node<T> ro;
+    ro.read(root, Bptfile);
+    if(ro.num == size){
+
+        Node<T> nro;
+        nro.num = 0;
+        nro.isLeaf = false;
+        nro.ptr[0] = root;
+
+        split(nro, ro, 0);
+
+        ro.write(root, Bptfile);
+        root = newNode();
+        nro.write(root, Bptfile);
+        //nro.print();
+    }
+    insert(val, addr, root);
+
+
+}
+
+template <typename T>
+void bPlusTree<T>::insert(T val, offset addr, offset cur){
+
+    int idx;
+    Node<T> r;
+    r.read(cur, Bptfile);
+
+    for(idx = 0; idx < r.num && r.key[idx] < val; idx++)
+        ;
+
+    if(idx < r.num && r.isLeaf && r.key[idx] == val){
+        cout << "duplicate key" <<endl;
+        return;
+    }
+    if( !r.isLeaf ){
+        Node<T> s;
+        s.read(r.ptr[idx], Bptfile);
+
+        if(s.num == size){
+            split(r, s, idx);
+            r.write(cur, Bptfile);
+            s.write(r.ptr[idx], Bptfile);
+        }
+
+        if(val < r.key[idx] || idx == r.num){
+            insert(val, addr, r.ptr[idx]);
+        }else{
+            insert(val, addr, r.ptr[idx+1]);
+        }
+    }else{
+        for(int j = r.num; j > idx; j--){
+            r.key[j] = r.key[j-1];
+            r.ptr[j] = r.ptr[j-1];
+        }
+        r.key[idx] = val;
+        r.ptr[idx] = addr;
+        r.num++;
+        r.write(cur, Bptfile);
+    }
+}
+
+
+
+template <typename T>
+void bPlusTree<T>::split(Node<T> &parent, Node<T> &cur, int childNum)
+{
+    int i;
+    int half = size/2;
+    for(i = parent.num; i > childNum; i--){
+        parent.key[i] = parent.key[i-1];
+        parent.ptr[i+1] = parent.ptr[i];
+    }
+    parent.num++;
+
+    Node<T> t;
+
+    offset address = newNode();
+
+    parent.key[childNum] = cur.key[half+1];
+    parent.ptr[childNum+1] = address;
+
+    for(i = half + 1; i < size; i++){
+        t.key[i - half - 1] = cur.key[i];
+        t.ptr[i - half - 1] = cur.ptr[i];
+    }
+
+    t.num = size - half - 1;
+    t.ptr[t.num] = cur.ptr[size];
+    t.isLeaf = cur.isLeaf;
+
+    cur.num = half;
+
+    if(cur.isLeaf){
+        cur.num++;
+        t.ptr[size] = cur.ptr[size];
+        cur.ptr[size] = address;
+    }
+
+    t.write(address, Bptfile);
+
+}
+
+template <typename T>
+void bPlusTree<T>::print(offset cur)
+{
+    Node<T> r;
+    r.read(cur, Bptfile);
+    r.print();
+    Node<T> t;
+    for(int i = 0; i <= r.num; i++){
+        printf("(%d, %d)\n", 1, i);
+        t.read(r.ptr[i],Bptfile);
+        if(t.num != 0)
+            t.print();
+
+        Node<T> s;
+        for(int j = 0; j <= t.num; j++){
+            printf("(%d, %d)\n", 2, j);
+            s.read(t.ptr[j], Bptfile);
+            if(s.num != 0)
+                s.print();
+        }
+        cout<<"--------------------"<<endl;
+    }
+
+}
+
+template <typename T>
+void bPlusTree<T>::printLeaf(){
+    Node<T> head;
+    head.read(root, Bptfile);
+    while(!head.isLeaf){
+        head.read(head.ptr[0],Bptfile);
+    }
+    while(1){
+        for(int i = 0; i < head.num; i++){
+            cout<<head.key[i]<<' ';
+        }
+        if(head.ptr[size] == 0){
+            break;
+        }
+        head.read(head.ptr[size], Bptfile);
+    }
+}
 template <typename T>
 pair<offset, offset> bPlusTree<T>::search(T val, Node<T> C)
 {
@@ -131,44 +278,6 @@ pair<offset, offset> bPlusTree<T>::search(T val, Node<T> C)
     }while(C.isLeaf == false);
     return make_pair(0, 0);
 
-
-
-}
-
-template <typename T>
-void bPlusTree<T>::insert(T val, offset addr)
-{
-    Node<T> r;
-    r.read(root, Bptfile);
-    int idx;
-    for(idx = 0; idx < r.num; idx++){
-        if(val < r.key[idx]) break;
-    }
-    cout<<"idx:"<<idx<<endl;
-    r.insert(val, addr, idx);
-    r.write(root, Bptfile);
-}
-
-
-
-template <typename T>
-void bPlusTree<T>::split(Node<T> &parent, Node<T> &current, int childNum)
-{
-
-}
-
-template <typename T>
-void bPlusTree<T>::print(offset cur)
-{
-    Node<T> r;
-    r.read(cur, Bptfile);
-
-    if(r.isLeaf == false)
-        r.print();
-
-    // for(int i = 0; i < r.size; i++){
-    //     print(r.ptr[i]);
-    // }
 }
 // template <typename T>
 // int insertInLeaf(Node L, T val, Node<T>* ptr)
--- test.cpp
+++ test.cpp
@@ -7,16 +7,22 @@ using namespace std;
 int main()
 {
     string path = "./data.txt";
-
+    int v[7] = {1,3,2,5,4,7,6};
+    int p[7] = {1001,1003,1002,1005,1004,1007,1006};
     bPlusTree<int> bpt(path);
 
+    // for(int i = 0; i < 5; i++){
+    //     bpt.insert(v[i], p[i]);
+    // }
+    //
+    // bpt.insert(7,1007);
+    //
+    for(int i = 1; i <= 22; i++){
+        bpt.insert(i, 1000+i);
+    }
+
     bpt.print(bpt.root);
-
-    bpt.insert(6,1006);
-    bpt.print(bpt.root);
-
-
-
+    //bpt.printLeaf();
 
 
 }
